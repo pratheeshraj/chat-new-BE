2b115af7e0884498aab777962b66aa46526fbec6
pratheeshraj <rajeshnsk5@gmail.com>
2023-02-27 19:46:18 +0530

    add

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = require('http').createServer(app);
 const PORT = 5001;
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://playful-taffy-4927bc.netlify.app',
     methods: ['GET', 'POST']
   }
 })
